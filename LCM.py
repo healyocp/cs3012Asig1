77f93e14c58dcc6b440b44a1716a9655337bfe3e
Portia Healy O'Connor <portiahealyoconnor@Portias-MacBook.local>
2018-10-03 16:47:59 +0100

    h

--- LCM.py
+++ LCM.py
@@ -1,1 +1,21 @@
 print("Greetings to internet citizens!")
+
+Class Node:
+
+    #constructor
+    def_init_(self,data):
+        self.data = data
+        self.left=None
+        self.right = None
+
+#functions to compare dif objects 
+
+     def __lt__(self, val):
+        return self.val < val
+    def __gt__(self, val):
+        return self.val > val
+
+     def __eq__(self, val):
+        return self.val == val
+     def __str__(self):
+        return "[Node val: %d]" % self.val
