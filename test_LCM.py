09edf2b42fdde3abcc43ab5fe4e5d56df9fa92af
Portia Healy O'Connor <portiahealyoconnor@Portias-MacBook.local>
2018-10-04 12:30:49 +0100

    Addition of test -> empty tree not working

--- test_LCM.py
+++ test_LCM.py
@@ -9,7 +9,13 @@ class Test(unittest.TestCase):
     def test_LCM_empty_tree(self):
         t=Tree()
         self.assertEqual(t.find_common(1,2), None)
-    
+    def test_LCM_small_tree(self):
+        t=Tree()
+        vals = [6,3,9]
+        for val in vals:
+            t.put(val)
+
+        self.assertEqual(t.find_common(3,9), 6)
 
 
 if __name__ == '__main__':
