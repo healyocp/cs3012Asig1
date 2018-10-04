3bf66097eb48f1173a6b77f43cbb6f914f75b0f9
Portia Healy O'Connor <portiahealyoconnor@Portias-MacBook.local>
2018-10-04 10:57:22 +0100

    learning about unit testing

---
+++ unitTestPractice.py
@@ -0,0 +1,16 @@
+import unittest
+
+from multiply import multiply
+class TestUM(unittest.TestCase):
+
+
+
+    def setUp(self):
+        pass
+
+    def test_numbers_3_4(self):
+        self.assertEqual( multiply(3,4), 12)
+
+
+if __name__ == '__main__':
+    unittest.main()
