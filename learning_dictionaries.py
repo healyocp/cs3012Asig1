f406996c5a1c2a55be27efe3fba368b31d96a055
Portia Healy O'Connor <portiahealyoconnor@Portias-MacBook.local>
2018-10-18 14:02:39 +0100

    more dictionaries

--- learning_dictionaries.py
+++ learning_dictionaries.py
@@ -1,6 +1,24 @@
+
+
+#A dictionary is a collection which is unordered,
+#changeable and indexed. In Python dictionaries are written with curly brackets,
+#and they have keys and values.
 thisdict =	{
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
 print(thisdict)
+
+
+x=thisdict.get("model")
+print (x)
+
+for x in thisdict:
+    print(x)
+
+for x in thisdict.values():
+    print(x)
+
+for x,y in thisdict.items():
+    print (x,y)
