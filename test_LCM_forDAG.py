30cffcd1112ce3f74e0dc3c44b35346b3ab213dd
Portia Healy O'Connor <portiahealyoconnor@Portias-MacBook.local>
2018-10-28 18:37:06 +0000

    addition of new functions

--- test_LCM_forDAG.py
+++ test_LCM_forDAG.py
@@ -58,6 +58,84 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(graphh.edges(), [{1, 2}, {1, 3}, {1, 12}, {2, 4}, {2, 5}, {3, 5}, {4, 6}, {5, 6}, {6, 7}])
         self.assertEqual(graphh.vertices(), [1, 2, 3, 4, 5, 6, 7, 12])
 
+
+    def test_LCM (self):
+        g = {   1: [2, 3],
+            2: [4, 5],
+            3: [5],
+            4: [6],
+            5: [6],
+            6: [7],
+            7: []}
+        graphh = graph(g)
+        self.assertEqual(graphh.findLCA(4,5),2)
+        self.assertEqual(graphh.findLCA(2,3),1)
+        self.assertEqual(graphh.findLCA(6,7),6)
+
+
+    def test_invert_dic(self):
+        g = {   1: [2, 3],
+            2: [4, 5],
+            3: [5],
+            4: [6],
+            5: [6],
+            6: [7],
+            7: []}
+        graphh = graph(g)
+        #invertedG=graphh.invert_dict_nonunique()
+        #self.assertEqual(invertedG.)
+
+    def test_bfs(self):
+            g = {   1: [2, 3],
+                2: [4, 5],
+                3: [5],
+                4: [6],
+                5: [6],
+                6: [7],
+                7: []}
+            graphh = graph(g)
+            """
+            self.assertEqual(graphh.bfs_connected_component(4),2)
+            self.assertEqual(graphh.bfs_connected_component(2),1)
+            self.assertEqual(graphh.bfs_connected_component(4),2)
+            self.assertEqual(graphh.bfs_connected_component(2),1)
+            self.assertEqual(graphh.bfs_connected_component(4),2)
+            self.assertEqual(graphh.bfs_connected_component(2),1)
+            self.assertEqual(graphh.bfs_connected_component(4),2)
+            self.assertEqual(graphh.bfs_connected_component(2),1)
+
+
+"""
+
+def test_cycle(self):
+        g = {   1: [2, 3],
+            2: [4, 5],
+            3: [5],
+            4: [6],
+            5: [6],
+            6: [7],
+            7: []}
+
+        g2 = {   1: [2, 3],
+            2: [4, 5],
+            3: [5],
+            4: [6],
+            5: [6],
+            6: [7],
+            7: [1]}
+        graphh = graph(g)
+        graphh2=graph(g2)
+
+        self.assertEqual(graphh.testCycle(),False)
+        self.assertEqual(graphh2.testCycle(),True)
+
+
+
+
+
+
+
+
 unittest.main(exit=False)
 
 if __name__ == '__main__':
